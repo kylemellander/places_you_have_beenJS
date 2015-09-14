d707783191bccbfaabc580c5c0ecb13435174cab
Clayton Easterbrooks & Kyle Mellander <student@epicodus.com>
2015-09-14 12:29:14 -0700

    add jquery to landmarks, basic styling changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
   $('select.dropdown').dropdown();
+
+  $("#add-landmark").click(function() {
+    $("#landmarks").append('<div class="eight wide field">' +
+                           '<input type="text" placeholder="Aztec Ruins" id="landmark">' +
+                           '</div>');
+  });
 })
